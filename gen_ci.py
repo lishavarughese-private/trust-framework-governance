9bf28c5991794dcc565c5c8bbdb0617b8c31f1c4
Lisha Varughese <lishavarughese@gmail.com>
2026-06-05 16:10:14 -0400

    Remove temp build scripts

--- gen_ci.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-content = open(sys.argv[1], 'r', encoding='utf-8').read()
-content_written = False
-
-# Read from write_yml_content.py which will have the YAML
-with open(sys.argv[2], 'w', encoding='utf-8') as f:
-    f.write(content)
